6269fd9a88a445befe8a49fe79fd2c2a2e3c4a40
Rachel Brendel <brendelrachel@gmail.com>
2023-02-05 19:52:30 -0600

    update code in restaurant.js and unskip all test in the restaurant-test.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,41 @@
+function createRestaurant(name) {
+var pizzaRestaurant = {
+  name: name,
+  menus: {
+    breakfast: [],
+    lunch: [],
+    dinner: []
+  }
+}
+return pizzaRestaurant
+}
+
+function addMenuItem(pizzaRestaurant, item) {
+  if (item.type === "breakfast" && pizzaRestaurant.menus.breakfast.includes(item) === false) {
+    pizzaRestaurant.menus.breakfast.push(item)
+  } else if (item.type === "lunch" && pizzaRestaurant.menus.lunch.includes(item) === false) {
+    pizzaRestaurant.menus.lunch.push(item)
+  } else if (item.type === "dinner" && pizzaRestaurant.menus.dinner.includes(item) === false) {
+    pizzaRestaurant.menus.dinner.push(item)
+} 
+}
+
+function removeMenuItem(pizzaRestaurant, item, type) {
+for (i = 0; i < pizzaRestaurant.menus[type].length; i++) {
+  if(pizzaRestaurant.menus[type][i].name === item) {
+    pizzaRestaurant.menus[type].splice(i, 1)
+    return `No one is eating our ${item} - it has been removed from the ${type} menu!`
+  }
+}
+return `Sorry, we don't sell ${item}, try adding a new recipe!`
+}
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem
 }
